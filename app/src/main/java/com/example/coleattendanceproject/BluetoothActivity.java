97f22cf403ffcfa686f42580ffbb0b9236722a10
4nthonyF4lcon <kaiseranthony22@gmail.com>
2023-03-27 13:54:00 -0500

    bluetooth multithread example code

--- app/src/main/java/com/example/coleattendanceproject/BluetoothActivity.java
+++ app/src/main/java/com/example/coleattendanceproject/BluetoothActivity.java
@@ -1,13 +1,17 @@
 package com.example.coleattendanceproject;
 
+import static android.content.ContentValues.TAG;
+import static android.provider.Telephony.Carriers.NAME;
+
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,7 +23,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.util.Set;
+import java.util.UUID;
 
 public class BluetoothActivity extends AppCompatActivity {
 
@@ -33,58 +40,51 @@ public class BluetoothActivity extends AppCompatActivity {
     BluetoothAdapter mBlueAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
 
         mStatusBlueTv = findViewById(R.id.statusBluetoothTv);
-        mPairedTv     = findViewById(R.id.pairedTv);
-        mBlueIv       = findViewById(R.id.bluetoothIv);
-        mOnBtn        = findViewById(R.id.onBtn);
-        mOffBtn       = findViewById(R.id.offBtn);
-        mDiscoverBtn  = findViewById(R.id.discoverableBtn);
-        mPairedBtn    = findViewById(R.id.pairedBtn);
+        mPairedTv = findViewById(R.id.pairedTv);
+        mBlueIv = findViewById(R.id.bluetoothIv);
+        mOnBtn = findViewById(R.id.onBtn);
+        mOffBtn = findViewById(R.id.offBtn);
+        mDiscoverBtn = findViewById(R.id.discoverableBtn);
+        mPairedBtn = findViewById(R.id.pairedBtn);
 
         //adapter
         mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
 
         //Check permissions for dangerous permissions
         //Bluetooth Advertise Permission
-        if(ContextCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED)
-        {
+        if (ContextCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(BluetoothActivity.this, new String[]{Manifest.permission.BLUETOOTH_ADVERTISE}, 2);
         }
         //Bluetooth Connect Permission
-        if(ContextCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)
-        {
+        if (ContextCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(BluetoothActivity.this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 2);
         }
         //Bluetooth Scan Permission
-        if(ContextCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED)
-        {
+        if (ContextCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(BluetoothActivity.this, new String[]{Manifest.permission.BLUETOOTH_SCAN}, 2);
         }
 
         //Course Location Permissions
-        if(ContextCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
+        if (ContextCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(BluetoothActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
         }
 
         //check if bluetooth is available
-        if (mBlueAdapter == null){
+        if (mBlueAdapter == null) {
             mStatusBlueTv.setText("Bluetooth is not available");
-        }
-        else{
+        } else {
             mStatusBlueTv.setText("Bluetooth is available");
         }
 
         //set image according to bluetooth status (on/off)
-        if(mBlueAdapter.isEnabled()){
+        if (mBlueAdapter.isEnabled()) {
             mBlueIv.setImageResource(R.drawable.ic_action_on);
-        }
-        else {
+        } else {
             mBlueIv.setImageResource(R.drawable.ic_action_off);
         }
 
@@ -97,8 +97,7 @@ public class BluetoothActivity extends AppCompatActivity {
                     //intent to on bluetooth
                     Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivityForResult(intent, REQUEST_ENABLE_BT);
-                }
-                else{
+                } else {
                     showToast("Bluetooth is already on");
                 }
             }
@@ -122,8 +121,7 @@ public class BluetoothActivity extends AppCompatActivity {
                     mBlueAdapter.disable();
                     showToast("Turning Bluetooth Off");
                     mBlueIv.setImageResource(R.drawable.ic_action_off);
-                }
-                else {
+                } else {
                     showToast("Bluetooth is already off");
                 }
             }
@@ -135,11 +133,10 @@ public class BluetoothActivity extends AppCompatActivity {
                 if (mBlueAdapter.isEnabled()) {
                     mPairedTv.setText("Paired Devices");
                     Set<BluetoothDevice> devices = mBlueAdapter.getBondedDevices();
-                    for (BluetoothDevice device: devices) {
-                        mPairedTv.append("\nDevice" + device.getName()+ "," + device);
+                    for (BluetoothDevice device : devices) {
+                        mPairedTv.append("\nDevice" + device.getName() + "," + device);
                     }
-                }
-                else {
+                } else {
                     // bluetooth is off so can't get paired devices
                     showToast("Turn on bluetooth to get paired devices");
                 }
@@ -153,12 +150,11 @@ public class BluetoothActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         switch (requestCode) {
             case REQUEST_ENABLE_BT:
-                if(resultCode == RESULT_OK) {
+                if (resultCode == RESULT_OK) {
                     //bluetooth is on
                     mBlueIv.setImageResource(R.drawable.ic_action_on);
                     showToast("Bluetooth is on");
-                }
-                else {
+                } else {
                     //user denied to turn bluetooth on
                     showToast("couldn't turn on bluetooth");
                 }
@@ -172,4 +168,46 @@ public class BluetoothActivity extends AppCompatActivity {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
-}
+    private class connectionThread extends Thread {
+        private final BluetoothServerSocket serverSocket;
+
+        public connectionThread(BluetoothServerSocket serverSocket) {
+            this.serverSocket = serverSocket;
+            // Use a temporary object that is later assigned to serverSocket
+            BluetoothServerSocket tmp = null;
+            try {
+                // APP_UUID is the app's UUID string
+                BluetoothAdapter bluetoothAdapter = null;
+                UUID APP_UUID = null;
+                tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, APP_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's listen() method failed", e);
+            }
+            serverSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            // Keep listening until exception occurs or a socket is returned.
+            while (true) {
+                try {
+                    socket = serverSocket.accept();
+                } catch (IOException e) {
+                    Log.e(TAG, "Socket's accept() method failed", e);
+                    break;
+                }
+                }
+            }
+        }
+
+        // Closes the connect socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                BufferedInputStream serverSocket = null;
+                serverSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the connect socket", e);
+            }
+        }
+    }
+
--- app/src/main/java/com/example/coleattendanceproject/MainActivity.java
+++ app/src/main/java/com/example/coleattendanceproject/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity
 {
     //Buttons
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
